9ccaa69ce2e0617d0d45778345cbfd8308a73fd4
吴奇生 <bjwuqisheng@corp.netease.com>
2017-10-26 19:46:25 +0800

    第一次提交

---
+++ main.js
@@ -0,0 +1,4 @@
+require('./test.less');
+const imageA = require('./images/a.png');
+
+console.log(1111);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = env => {
+	let publicPath = '';
+	if(env && env.NODE_ENV !== 'local') {
+		publicPath = '//www.examplg.com/';
+	}
+	return {
+		entry: './main.js',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.js',
+			publicPath
+		},
+		module: {
+			rules: [
+				{
+					test: /\.less$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: ['css-loader', 'postcss-loader', 'less-loader']
+					})
+				},
+				{
+					test:/\.(png|jpg|ttf|gif)$/,
+					use: [
+						{
+							loader: 'url-loader',
+							options: {
+								limit: 1,
+								name: 'images/[hash:8].[name].[ext]'
+							}
+						}
+					]
+				},
+				{ 
+					test: /\.(mp4|ogg|mp3|wav)$/, 
+					use: ['file-loader']
+				}
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './index.html'
+			}), new ExtractTextPlugin('test.css'), 
+			new UglifyJSPlugin()
+		],
+		devtool: 'inline-source-map'
+	}
+}
